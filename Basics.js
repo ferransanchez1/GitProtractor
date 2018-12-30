266db4e70a8cf04398405a6c6fe288930197b0d6
ferransanchez1 <ferransanchez1@hotmail.com>
2018-12-30 18:21:40 +0100

    Third commit about basics file

--- Basics.js
+++ Basics.js
@@ -8,10 +8,12 @@ if (a=="one")
 	console.log("the statement is true with one");
 	console.log("the statement is true with one second part");
 	console.log("the statement is true with one third part");
+	console.log("the statement is true with one four part");
 	}
 else if (a=="two")
 	{
 	console.log("the statement is true with two");
+	console.log("the statement is true with two second part");
 	}
 else if (a=="three")
 	{
